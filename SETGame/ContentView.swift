f613b7a9177f62f36a90a79357425443caa43ef3
Ben Howlett <ben@benhowlett.com>
2022-02-01 13:30:56 -0700

    Initial Commit

---
+++ SETGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SETGame
+//
+//  Created by Ben Howlett on 2022-02-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SETGame/SETGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SETGameApp.swift
+//  SETGame
+//
+//  Created by Ben Howlett on 2022-02-01.
+//
+
+import SwiftUI
+
+@main
+struct SETGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
